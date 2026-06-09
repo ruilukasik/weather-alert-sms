b8281af673d227d2a280f9eae7920cae6a24098c
ruilukasik <ruilukasik@gmail.com>
2026-06-09 16:44:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from twilio.rest import Client
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+
+weather_params = {
+    "lat": 38.722252,
+    "lon": -9.139337,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an ☔.",
+        from_="+17252378952",
+        to="+351963693017",
+    )
+    print(message.status)
